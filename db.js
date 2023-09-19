9b2fbe68f03ca858107c2d5144769cdd0ce7cdb0
juno-r-hyniva <juno.r@hyniva.com>
2023-09-19 21:26:21 +0530

    changing import

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 const db = new DynamoDBClient({});
 
 module.exports = db;
